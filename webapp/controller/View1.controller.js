539a75467ec15a96ab508338ebe1b0ea345e5006
GE341035 GE341035 <ge341035@sapexperienceacademy.com>
2026-08-12 10:08:14 +0000

    day 1

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,25 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
+], (Controller, JSONModel) => {
+    "use strict";
+
+    return Controller.extend("product.controller.View1", {
+        onInit() {
+            this.getView().byId('idButton').setEnabled(false);
+
+            var oViewModel = new JSONModel({
+                inputValue: ""
+            });
+            this.getView().setModel(oViewModel, "view");
+        },
+        onpress() {
+            var name = this.getView().byId('idName').getValue();
+            if (name != undefined) {
+                this.getView().byId('idHead').setText(`Hello ${name}`);
+                this.getView().byId('idButton').setIcon('sap-icon://arrow-left');
+            }
+
+        }
+    });
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"product/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
